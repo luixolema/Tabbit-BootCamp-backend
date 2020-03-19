d815b43468975ac4fffd9b231f36306165ba560f
Luis Ernesto Macias Avila <bootcamp04@tabit-gmbh.de>
2020-03-19 14:56:40 +0100

    New Infrestucture backend-frontend serpareted

---
+++ src/main/java/com/tabit/dcm2/Dcm2Application.java
@@ -0,0 +1,13 @@
+package com.tabit.dcm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dcm2Application.class, args);
+	}
+
+}
