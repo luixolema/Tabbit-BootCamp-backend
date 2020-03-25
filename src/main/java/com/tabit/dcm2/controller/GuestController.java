4908cf76fe69dd184b604f8c79705797a6876c00
Andrey Sapozhkov <bootcamp01@tabit-gmbh.de>
2020-03-25 11:52:25 +0100

    #DCM2-12 #DCM2-20 Add @CrossOrigin annotation

--- src/main/java/com/tabit/dcm2/controller/GuestController.java
+++ src/main/java/com/tabit/dcm2/controller/GuestController.java
@@ -4,11 +4,13 @@ import com.tabit.dcm2.service.GuestFilterType;
 import com.tabit.dcm2.service.GuestsDto;
 import com.tabit.dcm2.service.IGuestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class GuestController {
     @Autowired
     private IGuestService guestService;
