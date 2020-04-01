7cc6439479c470ea60db500129ce4c71f681664d
Yusef <bootcamp02@tabit-gmbh.de>
2020-04-01 17:41:39 +0200

    #DCM2-13 fix date format

---
+++ src/main/java/com/tabit/dcm2/config/DateAndTimeFormatConfig.java
@@ -0,0 +1,24 @@
+package com.tabit.dcm2.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class DateAndTimeFormatConfig {
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(DATE_TIME_FORMAT);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+        };
+    }
+}
