97f3c89219af7db9f601fc9bea4909ac26c3b209
Andrey Sapozhkov <bootcamp01@tabit-gmbh.de>
2020-04-09 16:31:12 +0200

    #DCM2-14 #DCM2-97 Fix missing setting for active flag in StayDto

--- src/main/java/com/tabit/dcm2/controller/util/MapperUtil.java
+++ src/main/java/com/tabit/dcm2/controller/util/MapperUtil.java
@@ -37,6 +37,7 @@ public class MapperUtil {
         stayDetails.setDivesAmount(stay.getDivesAmount());
         stayDetails.setNitrox(stay.isNitrox());
         stayDetails.setMedicalStatement(stay.isMedicalStatement());
+        stayDetails.setActive(stay.isActive());
         stayDetails.setPreBooking(stay.getPreBooking());
 
         StayDto stayDto = new StayDto();
