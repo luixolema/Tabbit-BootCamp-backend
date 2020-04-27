76c3ff2303b0da8c6fe32331cdf7e748f0dd6fa7
alexis <bootcamp03@tabit-gmbh,de>
2020-04-27 16:59:38 +0200

    #DCM2-15 #DCM2-132 GuestOverviewDto / GuestDto created by AbstractBuilder

--- src/main/java/com/tabit/dcm2/service/dto/GuestOverviewDto.java
+++ src/main/java/com/tabit/dcm2/service/dto/GuestOverviewDto.java
@@ -1,11 +1,14 @@
 package com.tabit.dcm2.service.dto;
 
 import com.tabit.dcm2.commons.AbstractBean;
+import com.tabit.dcm2.commons.AbstractNonNullValidatingBeanBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuestOverviewDto extends AbstractBean {
-    private List<GuestDto> guests;
+    private List<GuestDto> guests = new ArrayList<>();
     private int total;
 
     // default Costructor needed for json
@@ -23,5 +26,30 @@ public class GuestOverviewDto extends AbstractBean {
 
     public int getTotal() {
         return total;
+    }
+
+
+    public GuestOverviewDto copy() {
+        return new Builder()
+                .withGuests(getGuests().stream().map(GuestDto::copy).collect(Collectors.toList()))
+                .withTotal(getTotal())
+                .build();
+    }
+
+    public static class Builder extends AbstractNonNullValidatingBeanBuilder<GuestOverviewDto, Builder> {
+
+        public Builder() {
+            super(new GuestOverviewDto());
+        }
+
+        public Builder withGuests(List<GuestDto> guests) {
+            bean.guests = guests;
+            return this;
+        }
+
+        public Builder withTotal(int total) {
+            bean.total = total;
+            return this;
+        }
     }
 }
