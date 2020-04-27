45fe3129ea68055e2369ddefa80f41e8b9324404
Tuan Dao <tuan.dao@tabit-gmbh.de>
2020-04-27 10:12:36 +0200

    #DCM2-16 #DCM2-126 Review verify is not needed if there is a when which matches the verify (because if the when doesnt match there will be a nullpointer)

--- src/test/java/com/tabit/dcm2/controller/GuestControllerTest.java
+++ src/test/java/com/tabit/dcm2/controller/GuestControllerTest.java
@@ -174,7 +174,6 @@ public class GuestControllerTest {
         GuestPersonalDetailsDto guestPersonalDetailsDto = guestController.getGuestPersonalDetails(randomGuest.getId());
 
         // then
-        verify(guestService).getGuestById(randomGuest.getId());
         GuestMappingAssertions.assertPersonalDetails(randomGuest, guestPersonalDetailsDto);
     }
 }
