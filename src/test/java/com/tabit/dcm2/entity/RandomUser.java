0c7f7806080759a29b42d8c2eefde51a44cebc66
Yusef <bootcamp02@tabit-gmbh.de>
2020-05-07 21:20:00 +0200

    #DCM2-147 #DCM2-167 update tests

--- src/test/java/com/tabit/dcm2/entity/RandomUser.java
+++ src/test/java/com/tabit/dcm2/entity/RandomUser.java
@@ -1,11 +1,12 @@
 package com.tabit.dcm2.entity;
 
+import com.tabit.dcm2.service.dto.LoginDto;
 import com.tabit.dcm2.testutils.ValueProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class RandomUser {
-    public static User createRandomUser() {
+    public static User createRandomUserWithPassword(String password) {
         ValueProvider valueProvider = new ValueProvider();
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -13,14 +14,24 @@ public class RandomUser {
         user.setId(valueProvider.randomId());
         user.setName(valueProvider.randomString("name"));
         user.setLogin(valueProvider.randomEmail());
-        user.setPassword(passwordEncoder.encode(valueProvider.randomString("password")));
+        user.setPassword(passwordEncoder.encode(password));
 
         return user;
     }
 
-    public static User createRandomUserWithoutId() {
-        User user = createRandomUser();
+    public static User createRandomUserWithPasswordWithoutId(String password) {
+        User user = createRandomUserWithPassword(password);
         user.setId(null);
+
+        return user;
+    }
+
+    public static User createRandomUserFromLoginDto(LoginDto loginDto) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        User user = createRandomUserWithPasswordWithoutId(loginDto.getPassword());
+
+        user.setLogin(loginDto.getLogin());
+        user.setPassword(passwordEncoder.encode(loginDto.getPassword()));
 
         return user;
     }
--- src/test/java/com/tabit/dcm2/service/dto/RandomLoginDto.java
+++ src/test/java/com/tabit/dcm2/service/dto/RandomLoginDto.java
@@ -1,6 +1,5 @@
 package com.tabit.dcm2.service.dto;
 
-import com.tabit.dcm2.entity.User;
 import com.tabit.dcm2.testutils.ValueProvider;
 
 public class RandomLoginDto {
@@ -14,12 +13,5 @@ public class RandomLoginDto {
 
     public static LoginDto createRandomLoginDto() {
         return createRandomLoginDtoBuilder().build();
-    }
-
-    public static LoginDto createRandomLoginDtoFromUser(User user) {
-        return new LoginDto.Builder()
-                .withLogin(user.getLogin())
-                .withPassword(user.getPassword())
-                .build();
     }
 }
