e7b9f6e90a58fe32d84b0264aa15d4286216e3a8
Yusef <bootcamp02@tabit-gmbh.de>
2020-05-13 09:33:37 +0200

    #DCM2-147 #DCM2-179 change the hashing algorithm

--- src/main/java/com/tabit/dcm2/Dcm2Application.java
+++ src/main/java/com/tabit/dcm2/Dcm2Application.java
@@ -3,7 +3,7 @@ package com.tabit.dcm2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
@@ -11,7 +11,7 @@ public class Dcm2Application {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new MessageDigestPasswordEncoder("SHA-256");
     }
 
 	public static void main(String[] args) {
